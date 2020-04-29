a6ca20d505f782013bf82d66156913dd3038a2f7
pkpraveen2006 <64510750+pkpraveen2006@users.noreply.github.com>
2020-04-29 16:15:34 +0530

    Create Cachemat.R

---
+++ Cachemat.R
@@ -0,0 +1,42 @@
+makeCacheMatrix <- function(x = matrix())
+{
+  m <- NULL
+  set <- function(y) 
+  {
+    x <<- y   
+    m <<- NULL 
+  }
+  
+  get <- function() 
+  {
+    x
+  }
+  
+  setInverse <- function(inverse)
+  {
+    m <<- inverse
+  }
+  
+  getInverse <- function()
+  {
+    m
+  }   
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+cacheSolve <- function(x)
+{
+  m <- x$getInverse()
+  if(!is.null(m)) 
+  { 
+    message("Attaining cached data")
+    return(m)
+  }
+  
+  data <- x$get()  
+  m <- solve(data) 
+  x$setInverse(m) 
+  m                
+}
